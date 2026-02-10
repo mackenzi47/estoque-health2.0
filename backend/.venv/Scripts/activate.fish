74c6b339b0015596c4bd8d1b399a9a22fd619681
Lucas Franco <lroberto989@gmail.com>
2026-02-10 14:10:17 -0300

    deploy inicial

--- backend/.venv/Scripts/activate.fish
+++ backend/.venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\lucas\Downloads\estoque-health\estoque-health\backend\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\lucas\estoque-health\backend\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
