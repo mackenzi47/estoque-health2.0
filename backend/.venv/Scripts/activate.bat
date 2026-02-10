7ff8f18ea2059a390d8071bd69f7993bbf29de6c
Lucas Franco <lroberto989@gmail.com>
2026-02-10 13:09:40 -0300

    Remove virtualenv e ajusta gitignore

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\saidp\OneDrive\Área de Trabalho\One Drive\OneDrive\Documents\estoque-health\backend\.venv"
+set "VIRTUAL_ENV=C:\Users\lucas\Downloads\estoque-health\estoque-health\backend\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
